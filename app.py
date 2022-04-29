71aabfcc04815e548afda874b5f43346e1d2e59e
IgnatiusEzeani <igezeani@yahoo.com>
2022-04-29 14:18:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,12 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
 
-st.title('Text Summarization Demo')
-st.markdown('Using mT5 transformer model')
-model = st.selectbox('Select the model', ('mT5'))
-
-article_text = """Videos that say approved vaccines are dangerous and cause autism, cancer or infertility are among those that will be taken down, the company said.  The policy includes the termination of accounts of anti-vaccine influencers.  Tech giants have been criticised for not doing more to counter false health information on their sites.  In July, US President Joe Biden said social media platforms were largely responsible for people's scepticism in getting vaccinated by spreading misinformation, and appealed for them to address the issue.  YouTube, which is owned by Google, said 130,000 videos were removed from its platform since last year, when it implemented a ban on content spreading misinformation about Covid vaccines.  In a blog post, the company said it had seen false claims about Covid jabs "spill over into misinformation about vaccines in general". The new policy covers long-approved vaccines, such as those against measles or hepatitis B.  "We're expanding our medical misinformation policies on YouTube with new guidelines on currently administered vaccines that are approved and confirmed to be safe and effective by local health authorities and the WHO," the post said, referring to the World Health Organization."""
-
-input_text = st.text_area('Text Input', article_text)
-
-def run_model(input_text):
+def run_model(text):
     model_name = "csebuetnlp/mT5_multilingual_XLSum"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-    input_ids = tokenizer( [WHITESPACE_HANDLER(article_text)],
+    input_ids = tokenizer([WHITESPACE_HANDLER(text)],
         return_tensors="pt",
         padding="max_length",
         truncation=True,
@@ -36,9 +28,19 @@ def run_model(input_text):
         skip_special_tokens=True,
         clean_up_tokenization_spaces=False
     )
+    
+st.title('Text Summarization Demo')
+st.markdown('Using mT5 transformer model')
+model = st.selectbox('Select the model', ('mT5'))
+
+article_text = """Videos that say approved vaccines are dangerous and cause autism, cancer or infertility are among those that will be taken down, the company said.  The policy includes the termination of accounts of anti-vaccine influencers.  Tech giants have been criticised for not doing more to counter false health information on their sites.  In July, US President Joe Biden said social media platforms were largely responsible for people's scepticism in getting vaccinated by spreading misinformation, and appealed for them to address the issue.  YouTube, which is owned by Google, said 130,000 videos were removed from its platform since last year, when it implemented a ban on content spreading misinformation about Covid vaccines.  In a blog post, the company said it had seen false claims about Covid jabs "spill over into misinformation about vaccines in general". The new policy covers long-approved vaccines, such as those against measles or hepatitis B.  "We're expanding our medical misinformation policies on YouTube with new guidelines on currently administered vaccines that are approved and confirmed to be safe and effective by local health authorities and the WHO," the post said, referring to the World Health Organization."""
+
+input_text = st.text_area('Text Input', article_text)
+
+
 
 if st.button('Submit'):
-    st.write(run_model(text))
+    st.write(run_model(input_text))
 
 #=======================================================================
 # import torch
