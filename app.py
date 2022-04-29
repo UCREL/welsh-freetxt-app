5d4b0637dcfb535ad5a0992e490bf44993c3869f
IgnatiusEzeani <igezeani@yahoo.com>
2022-04-29 15:42:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,51 @@
 import streamlit as st
-import sentencepiece
-import re
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from txtai.pipeline import Summary
+
+# import sentencepiece
+# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 st.title('Text Summarization Demo')
 st.markdown('Using mT5 transformer model')
 # model = st.selectbox('Select the model', ('', 'mT5'))
 
 
-WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
+# WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
 
-def run_model(text):
-    model_name = "csebuetnlp/mT5_multilingual_XLSum"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+# def run_model(text):
+    # model_name = "csebuetnlp/mT5_multilingual_XLSum"
+    # tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-    input_ids = tokenizer([WHITESPACE_HANDLER(text)],
-        return_tensors="pt",
-        padding="max_length",
-        truncation=True,
-        max_length=512
-    )["input_ids"]
+    # input_ids = tokenizer([WHITESPACE_HANDLER(text)],
+        # return_tensors="pt",
+        # padding="max_length",
+        # truncation=True,
+        # max_length=512
+    # )["input_ids"]
 
-    output_ids = model.generate(
-        input_ids=input_ids,
-        max_length=84,
-        no_repeat_ngram_size=2,
-        num_beams=4
-    )[0]
+    # output_ids = model.generate(
+        # input_ids=input_ids,
+        # max_length=84,
+        # no_repeat_ngram_size=2,
+        # num_beams=4
+    # )[0]
 
-    summary = tokenizer.decode(
-        output_ids,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False
-    )
+    # summary = tokenizer.decode(
+        # output_ids,
+        # skip_special_tokens=True,
+        # clean_up_tokenization_spaces=False
+    # )
     
-    return(summary)
+    # return(summary)
 
 article_text = """Videos that say approved vaccines are dangerous and cause autism, cancer or infertility are among those that will be taken down, the company said.  The policy includes the termination of accounts of anti-vaccine influencers.  Tech giants have been criticised for not doing more to counter false health information on their sites.  In July, US President Joe Biden said social media platforms were largely responsible for people's scepticism in getting vaccinated by spreading misinformation, and appealed for them to address the issue.  YouTube, which is owned by Google, said 130,000 videos were removed from its platform since last year, when it implemented a ban on content spreading misinformation about Covid vaccines.  In a blog post, the company said it had seen false claims about Covid jabs "spill over into misinformation about vaccines in general". The new policy covers long-approved vaccines, such as those against measles or hepatitis B.  "We're expanding our medical misinformation policies on YouTube with new guidelines on currently administered vaccines that are approved and confirmed to be safe and effective by local health authorities and the WHO," the post said, referring to the World Health Organization."""
 
 input_text = st.text_area('Text Input', article_text)
 
+# Create summary model
+summary = Summary('csebuetnlp/mT5_multilingual_XLSum')
+
 if st.button('Submit'):
-    st.write(run_model(input_text))
+    st.write(summary(article_text, 200))
+
+# st.write(run_model(input_text))
