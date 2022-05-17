e14dbabdbb30b2cf1b60b765b060d2a765e233d1
IgnatiusEzeani <igezeani@yahoo.com>
2022-05-17 08:49:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -116,6 +116,36 @@ def run_text_summarizer():
         else:
             input_text = st.text_area('Type or paste your text into the text box:', '<Please enter your text...>')
 
+
+
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+         # To read file as bytes:
+         bytes_data = uploaded_file.getvalue()
+         st.write(bytes_data)
+
+         # To convert to a string based IO:
+         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+         st.write(stringio)
+
+         # To read file as string:
+         string_data = stringio.read()
+         st.write(string_data)
+
+         # Can be used wherever a "file-like" object is accepted:
+         dataframe = pd.read_csv(uploaded_file)
+         st.write(dataframe)
+
+
+
+
+
+
+
+
+
+
+
         chosen_ratio = st.sidebar.slider('Select summary ratio [10% to 50%]',  min_value=10, max_value=50, step=10)/100
         if st.button("Summarise👈"):
             if input_text and input_text!='<Please enter your text...>':
