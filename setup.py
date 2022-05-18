2954c5c55bd42c82758e4ca41d2acd81ccd7bc7e
IgnatiusEzeani <igezeani@yahoo.com>
2022-05-18 13:17:09 +0100

    update app

--- setup.py
+++ setup.py
@@ -7,46 +7,13 @@ import nltk
 import networkx as nx
 nltk.download('punkt') # one time execution
 from nltk.tokenize import sent_tokenize
-from lexrank import LexRank
 from summa.summarizer import summarize as summa_summarizer
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 ## Define summarizer models
-# lex_rank
-def lex_rank_summarize(article, ratio=0.5):
-  sentences = sent_tokenize(article)
-  summary = LexRank(sentences).get_summary(sentences,
-                             summary_size=int(len(sentences)*0.5), threshold=.1)
-  return "\n".join(summary)
-
 # text_rank
 def text_rank_summarize(article, ratio):
   return summa_summarizer(article, ratio=ratio)
 
-# text_rank
-# def text_rank_summarize(article, ratio=0.5):
-  # return summa_summarizer(article, ratio=ratio)
-
-# Define Topline summarizers
-def tfidf_summarize(article, ratio=0.5):
-  sentences = sent_tokenize(article)
-  # get similarity matrix
-  sim_mat = cosine_similarity(TfidfVectorizer().fit_transform(sentences))
-  scores = nx.pagerank_numpy(nx.from_numpy_array(sim_mat))
-  top_ranked = sorted(scores.items(), key=lambda x: x[1], 
-                      reverse=True)[:int(len(scores)*ratio)]
-  summary = [sentences[i] for i,_ in top_ranked]
-  return "\n".join(summary)
-
-# build similarity matrix
-def gen_similarity_matrix(sents):
-  sim_mat = np.zeros([len(sents), len(sents)])
-  for i in range(len(sents)):
-    for j in range(len(sents)):
-      if i != j:
-        sim_mat[i][j] = float(cosine_similarity(sents[i], sents[j]))
-  return sim_mat
 
 #helper functions------------------------------------------------------------------
 def uploadfile():
@@ -98,6 +65,7 @@ def run_text_summarizer():
                     st.write(sent_tokenize(text_rank_summarize(input_text, ratio=0.5))[0])
             else:
                 st.write("Rhowch eich testun...(Please enter your text...)")
+
     else: #English
         st.markdown('## 🌷 Welsh Summary Creator')
         st.markdown("### Enter your text below:")
