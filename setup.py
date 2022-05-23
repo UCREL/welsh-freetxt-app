db51d4e765d43001e324af8fdd0db2a108e6a030
IgnatiusEzeani <igezeani@yahoo.com>
2022-05-24 00:22:56 +0100

    add welsh stopwords

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ from summa.summarizer import summarize as summa_summarizer
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 nltk.download('punkt') # one time execution
 
-
-
+STOPWORDS = STOPWORDS + open('welsh_stopwords.txt', 'r',
+                                            encoding='utf8').read()
 
 #📃📌📈📈📉⛱🏓🏆🎲
 
@@ -24,7 +24,7 @@ def text_rank_summarize(article, ratio):
   return summa_summarizer(article, ratio=ratio)
 
 
-#helper functions------------------------------------------------------------------
+#helper functions---------------------------------------------------
 
 #--------uploading file ---------------
 def uploadfile():
@@ -51,49 +51,6 @@ def get_kwic(text, keyword, window_size=1, maxInstances=10, lower_case=False):
         right_context = ' '.join(tokens[index+1:index+window_size+1])
         kwic_insts.append((left_context, target_word, right_context))
     return kwic_insts
-
-def cloud(image, text, max_word, max_font, random):
-    stopwords = set(STOPWORDS)
-    stopwords.update(['us', 'one', 'will', 'said', 'now', 'well', 'man', 'may', 'little', 'say', 'must', 'way', 'long', 'yet', 'mean', 'put', 'seem', 'asked', 'made', 'half', 'much',
-    'certainly', 'might', 'came'])
-    
-    wc = WordCloud(background_color="white", colormap="hot", max_words=max_word, mask=image,
-    stopwords=stopwords, max_font_size=max_font, random_state=random)
-
-    # generate word cloud
-    wc.generate(text)
-
-    # create coloring from image
-    image_colors = ImageColorGenerator(image)
-
-    # show the figure
-    plt.figure(figsize=(100,100))
-    fig, axes = plt.subplots(1,2, gridspec_kw={'width_ratios': [3, 2]})
-    axes[0].imshow(wc, interpolation="bilinear")
-    # recolor wordcloud and show
-    # we could also give color_func=image_colors directly in the constructor
-    axes[1].imshow(image, cmap=plt.cm.gray, interpolation="bilinear")
-
-    for ax in axes:
-        ax.set_axis_off()
-    st.pyplot()
-
-def main():
-    st.write("# Text Summarization with a WordCloud")
-    st.write("[By Boadzie Daniel](https://boadzie.surge.sh)")
-    max_word = st.sidebar.slider("Max words", 200, 3000, 200)
-    max_font = st.sidebar.slider("Max Font Size", 50, 350, 60)
-    random = st.sidebar.slider("Random State", 30, 100, 42 )
-    image = st.file_uploader("Choose a file(preferably a silhouette)")
-    text = st.text_area("Add text ..")
-    if image and text is not None:
-        if st.button("Plot"):
-            st.write("### Original image")
-            image = np.array(Image.open(image))
-            # st.image(image, width=100, use_column_width=True)
-            st.write("### Word cloud")
-            st.write(cloud(image, text, max_word, max_font, random), use_column_width=True)
-
     
 #apps------------------------------------------------------------------
 def run_text_summarizer():
