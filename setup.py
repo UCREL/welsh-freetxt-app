d8fe26d1d9a5d3995c10a03480f2e07720c9e550
IgnatiusEzeani <igezeani@yahoo.com>
2022-05-23 20:37:58 +0100

    update `requirements.txt` and `setup.py`

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 import string
+import streamlit_wordcloud as wordcloud
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -171,13 +172,32 @@ def run_visualizer():
     col2.subheader("Word Cloud")
     arr = np.random.normal(1, 1, size=100)
     fig, ax = plt.subplots()
-    ax.hist(arr, bins=20)
-    col2.pyplot(w_cloud)
 
-    #Set figure size
-    plt.figure(figsize=(40, 30))
-    # Display image
-    plt.imshow(w_cloud) 
-    # No axis 
-    plt.axis("off")
-    col2.plt.show(w_cloud)
+    words = [
+        dict(text="Robinhood", value=16000, color="#b5de2b", country="US", industry="Cryptocurrency"),
+        dict(text="Personio", value=8500, color="#b5de2b", country="DE", industry="Human Resources"),
+        dict(text="Boohoo", value=6700, color="#b5de2b", country="UK", industry="Beauty"),
+        dict(text="Deliveroo", value=13400, color="#b5de2b", country="UK", industry="Delivery"),
+        dict(text="SumUp", value=8300, color="#b5de2b", country="UK", industry="Credit Cards"),
+        dict(text="CureVac", value=12400, color="#b5de2b", country="DE", industry="BioPharma"),
+        dict(text="Deezer", value=10300, color="#b5de2b", country="FR", industry="Music Streaming"),
+        dict(text="Eurazeo", value=31, color="#b5de2b", country="FR", industry="Asset Management"),
+        dict(text="Drift", value=6000, color="#b5de2b", country="US", industry="Marketing Automation"),
+        dict(text="Twitch", value=4500, color="#b5de2b", country="US", industry="Social Media"),
+        dict(text="Plaid", value=5600, color="#b5de2b", country="US", industry="FinTech"),
+    ]
+    return_obj = wordcloud.visualize(words, tooltip_data_fields={
+        'text':'Company', 'value':'Mentions', 'country':'Country of Origin', 'industry':'Industry'
+    }, per_word_coloring=False)
+    
+    
+    # ax.hist(arr, bins=20)
+    # col2.pyplot(w_cloud)
+
+    # #Set figure size
+    # plt.figure(figsize=(40, 30))
+    # # Display image
+    # plt.imshow(w_cloud) 
+    # # No axis 
+    # plt.axis("off")
+    # col2.plt.show(w_cloud)
