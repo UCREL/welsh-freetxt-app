5dbaf0ba63708dcdc3117d1c3d0d3e5babb9c8ff
IgnatiusEzeani <igezeani@yahoo.com>
2022-05-25 15:09:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -183,7 +183,7 @@ def run_visualizer():
         st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
         st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
         color = st.radio('Switch image colour:', ('Color', 'Black'))
-        img_cols = 'black' if color == 'Black' else None
+        img_cols = ImageColorGenerator(mask) if color == 'Black' else None
         
         # image_colors = ImageColorGenerator(mask)
         plt.figure(figsize=[20,15])
