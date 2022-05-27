a2d44871eec22ab192ba62c806d194852bf2d359
IgnatiusEzeani <igezeani@yahoo.com>
2022-05-27 08:57:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from nltk.corpus import stopwords
 en_stopwords = list(stopwords.words('english'))
 cy_stopwords = open('welsh_stopwords.txt', 'r', encoding='utf8').read().split('\n')
 STOPWORDS = set(en_stopwords + cy_stopwords)
-PUNCS = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+PUNCS = '''!→()-[]{};:'"\,<>./?@#$%^&*_~'''
 
 
 
@@ -88,10 +88,8 @@ def run_summarizer():
         st.markdown("#### Rhowch eich testun isod:")
         option = st.sidebar.radio('Sut ydych chi am fewnbynnu eich testun?', ('Defnyddiwch destun enghreifftiol', 'Rhowch eich testun eich hun', 'Llwythwch ffeil testun i fyny'))
         if option == 'Defnyddiwch destun enghreifftiol':
-           example_fname = st.sidebar.selectbox('Select example text:', [f for f in os.listdir('example_texts')
-                                                  if f.startswith('cy')])
-           # example_fname = st.sidebar.selectbox('Select example text:', ['cy_ex_0_Dulyn', 'cy_ex_1_Menter Iaith Môn', 'cy_ex_2_Pencampwriaeth', 'cy_ex_3_Paris',
-           # 'cy_ex_4_Neuadd y Ddinas', 'cy_ex_5_Y_Gofid_Mawr_Covid19'])
+           example_fname = st.sidebar.selectbox('Select example text:', sorted([f for f in os.listdir('example_texts')
+                                                  if f.startswith('cy')]))
 
            with open(os.path.join('example_texts', example_fname), 'r', encoding='utf8') as example_file:
                example_text = example_file.read()
@@ -121,8 +119,8 @@ def run_summarizer():
         st.markdown("#### Enter your text below:")
         option = st.sidebar.radio('How do you want to input your text?', ('Use an example text', 'Paste a copied', 'Upload a text file'))
         if option == 'Use an example text':           
-           example_fname = st.sidebar.selectbox('Select example text:', [f for f in os.listdir('example_texts')
-                                                  if f.startswith('en')])
+           example_fname = st.sidebar.selectbox('Select example text:', sorted([f for f in os.listdir('example_texts')
+                                                  if f.startswith('en')]))
            # example_fname = st.sidebar.selectbox('Select example text:', ['example_welsh_wikipedia.txt'])
            # example_fname = st.sidebar.selectbox('Select example text:', ['en_ex_0_Castell Coch', 'en_ex_1_Beaumaris Castle', 'en_ex_2_Blaenavon Ironworks', 'en_ex_3_Caerleon Roman Baths',
            # 'en_ex_4_Caernarfon Castle', 'en_ex_5_Caerphilly Castle'])
@@ -162,12 +160,8 @@ def run_visualizer():
     # st.markdown('### 🔍 Visualization')
     option = st.sidebar.radio('How do you want to input your text?', ('Use an example text', 'Paste a copied', 'Upload a text file'))
     if option == 'Use an example text':
-       # example_fname = st.sidebar.selectbox('Select example text:', ['cy_ex_0_Dulyn', 'cy_ex_1_Menter Iaith Môn', 'cy_ex_2_Pencampwriaeth', 'cy_ex_3_Paris',
-       # 'cy_ex_4_Neuadd y Ddinas', 'cy_ex_5_Y_Gofid_Mawr_Covid19'])
        
-       example_fname = st.sidebar.selectbox('Select example text:', os.listdir('example_texts')) #['example_welsh_wikipedia.txt']
-       # 'en_ex_1_Beaumaris Castle', 'en_ex_2_Blaenavon Ironworks', 'en_ex_3_Caerleon Roman Baths',
-       # 'en_ex_4_Caernarfon Castle', 'en_ex_5_Caerphilly Castle'])
+       example_fname = st.sidebar.selectbox('Select example text:', sorted(os.listdir('example_texts'))) #['example_welsh_wikipedia.txt']
        
        with open(os.path.join('example_texts', example_fname), 'r', encoding='utf8') as example_file:
            example_text = example_file.read()
