7ae36ac64f67db3a5b4480dea8140522a7ba1947
IgnatiusEzeani <igezeani@yahoo.com>
2022-06-07 19:06:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -250,13 +250,6 @@ def run_visualizer():
                 # You can call any Streamlit command, including custom components:
                 st.bar_chart(np.random.randn(50, 3))
 
-            with st.container():
-                st.write("This is inside the container")
-
-                # You can call any Streamlit command, including custom components:
-                st.bar_chart(np.random.randn(50, 3))
-    
-
     with col1:
         st.markdown("**Word Cloud**")
         with st.expander("ℹ️ - Settings", expanded=False):
@@ -345,7 +338,6 @@ def run_visualizer():
                     colloc_str = ', '.join([f"{w}[{c}]" for w, c in collocs])
                     st.write(f"Collocations for '{keyword}':\n{colloc_str}")
                     plot_collocation(keyword, collocs)
-
 
 # def run_analyze():
     # with st.expander("ℹ️ - About Analyzer", expanded=False):
