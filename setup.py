9ddec629d6106968f823258a8848c206f619dacf
IgnatiusEzeani <igezeani@yahoo.com>
2022-06-07 23:16:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -362,66 +362,66 @@ def run_visualizer():
 # Borrowed from https://github.com/streamlit/example-app-bert-keyword-extractor
     st.markdown("**Keyword/KeyPhrase Extraction**")
     with st.expander("ℹ️ - Settings", expanded=False):
-        ce, c1, ce, c2, c3 = st.columns([0.07, 1, 0.07, 5, 0.07])
-        with c1:
-            model_type = st.radio( "Choose your model", ["DistilBERT (Default)", "Flair"],
-            help="Only the DistilBERT works for now!",
-        )
-            if model_type == "Default (DistilBERT)":
-            # kw_model = KeyBERT(model=roberta)
-                @st.cache(allow_output_mutation=True)
-                def load_model():
-                    return KeyBERT(model=roberta)
-                kw_model = load_model()
-            else:
-                @st.cache(allow_output_mutation=True)
-                def load_model():
-                    return KeyBERT("distilbert-base-nli-mean-tokens")
-                kw_model = load_model()
+        with st.form(key="my_form"):
+            ce, c1, ce, c2, c3 = st.columns([0.07, 1, 0.07, 5, 0.07])
+            with c1:
+                model_type = st.radio( "Choose your model", ["DistilBERT (Default)", "Flair"],
+                help="Only the DistilBERT works for now!",
+            )
+                if model_type == "Default (DistilBERT)":
+                # kw_model = KeyBERT(model=roberta)
+                    @st.cache(allow_output_mutation=True)
+                    def load_model():
+                        return KeyBERT(model=roberta)
+                    kw_model = load_model()
+                else:
+                    @st.cache(allow_output_mutation=True)
+                    def load_model():
+                        return KeyBERT("distilbert-base-nli-mean-tokens")
+                    kw_model = load_model()
 
-            top_N = st.slider(
-                "# of results",
-                min_value=1,
-                max_value=30,
-                value=10,
-                help="You can choose the number of keywords/keyphrases to display. Between 1 and 30, default number is 10.",
-            )
-            min_Ngrams = st.number_input(
-                "Minimum Ngram",
-                min_value=1,
-                max_value=4,
-                help="The minimum value for the ngram range. *Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases. To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases."
-            )
+                top_N = st.slider(
+                    "# of results",
+                    min_value=1,
+                    max_value=30,
+                    value=10,
+                    help="You can choose the number of keywords/keyphrases to display. Between 1 and 30, default number is 10.",
+                )
+                min_Ngrams = st.number_input(
+                    "Minimum Ngram",
+                    min_value=1,
+                    max_value=4,
+                    help="The minimum value for the ngram range. *Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases. To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases."
+                )
 
-            max_Ngrams = st.number_input(
-                "Maximum Ngram",
-                value=2,
-                min_value=1,
-                max_value=4,
-                help="The maximum value for the keyphrase_ngram_range. *Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases. To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases."
-            )
+                max_Ngrams = st.number_input(
+                    "Maximum Ngram",
+                    value=2,
+                    min_value=1,
+                    max_value=4,
+                    help="The maximum value for the keyphrase_ngram_range. *Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases. To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases."
+                )
 
-            StopWordsCheckbox = st.checkbox(
-                "Remove stop words",
-                help="Tick this box to remove stop words from the document (currently English only)",
-            )
+                StopWordsCheckbox = st.checkbox(
+                    "Remove stop words",
+                    help="Tick this box to remove stop words from the document (currently English only)",
+                )
 
-            use_MMR = st.checkbox(
-                "Use MMR",
-                value=True,
-                help="You can use Maximal Margin Relevance (MMR) to diversify the results. It creates keywords/keyphrases based on cosine similarity. Try high/low 'Diversity' settings below for interesting variations.",
-            )
+                use_MMR = st.checkbox(
+                    "Use MMR",
+                    value=True,
+                    help="You can use Maximal Margin Relevance (MMR) to diversify the results. It creates keywords/keyphrases based on cosine similarity. Try high/low 'Diversity' settings below for interesting variations.",
+                )
 
-            Diversity = st.slider(
-                "Keyword diversity (MMR only)",
-                value=0.5,
-                min_value=0.0,
-                max_value=1.0,
-                step=0.1,
-                help="The higher the setting, the more diverse the keywords. Note that the *Keyword diversity* slider only works if the *MMR* checkbox is ticked."
-            )
-        with c2:
-            with st.form(key="my_form"):
+                Diversity = st.slider(
+                    "Keyword diversity (MMR only)",
+                    value=0.5,
+                    min_value=0.0,
+                    max_value=1.0,
+                    step=0.1,
+                    help="The higher the setting, the more diverse the keywords. Note that the *Keyword diversity* slider only works if the *MMR* checkbox is ticked."
+                )
+            with c2:
                 MAX_WORDS = 500
                 res = len(re.findall(r"\w+", input_text))
                 if res > MAX_WORDS:
