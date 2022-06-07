23190e302ffc611bcedbd2d8bf0723094092ddae
IgnatiusEzeani <igezeani@yahoo.com>
2022-06-07 19:01:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -341,6 +341,18 @@ with st.container():
     # You can call any Streamlit command, including custom components:
     st.bar_chart(np.random.randn(50, 3))
 
+with st.container():
+    st.write("This is inside the container")
+
+    # You can call any Streamlit command, including custom components:
+    st.bar_chart(np.random.randn(50, 3))
+    
+with st.container():
+    st.write("This is inside the container")
+
+    # You can call any Streamlit command, including custom components:
+    st.bar_chart(np.random.randn(50, 3))
+
 st.write("This is outside the container")
 
 
