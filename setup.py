ef4933d1f40991bdac6e577a9ba2d72a1c837adb
IgnatiusEzeani <igezeani@yahoo.com>
2022-08-25 14:28:43 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ nltk.download('punkt') # one time execution
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
 random.seed(10)
+from labels import MESSAGES
+
 # For keyword extraction
 # For Flair (Keybert) ToDo
 # from flair.embeddings import TransformerDocumentEmbeddings
@@ -39,12 +41,9 @@ PUNCS = '''!→()-[]{};:'"\,<>./?@#$%^&*_~'''
 EXAMPLES_DIR = 'example_texts_pub'
 # EXAMPLES_DIR = 'example_texts_cadw'
 
-## Define summarizer models
 # text_rank
 def text_rank_summarize(article, ratio):
   return summa_summarizer(article, ratio=ratio)
-
-#helper functions---------------------------------------------------
 
 #------------------------- uploading file ---------------------------
 def uploadfile():
@@ -210,73 +209,26 @@ def plot_sentiments(data, fine_grained=True):
   st.set_option('deprecation.showPyplotGlobalUse', False)
   st.pyplot()
 
-#apps------------------------------------------------------------------
+#-------------apps-----------------------------
 def run_summarizer():
     language = st.sidebar.selectbox('Newid iaith (Change language):', ['Cymraeg', 'English'])
-    with st.expander("ℹ️ - About this app", expanded=False):
-        st.markdown(
-            """     
-            -   This tool adapts the app from the [Welsh Summarization] (https://github.com/UCREL/welsh-summarization-dataset) project!
-            -   It performs simple extractive summarisation with the [TextRank]() alrogithm.
-            """
-        )
+    lang = 'cy' if language == 'Cymraeg' else 'en'
+    st.markdown(MESSAGES[f'{lang}.ext.md'])
+    with st.expander(MESSAGES[f'{lang}.info.title'], expanded=False):
+        st.markdown(MESSAGES[f'{lang}.md'])
+    option = st.sidebar.radio(MESSAGES[lang][7], (MESSAGES[lang][8], MESSAGES[lang][9], MESSAGES[lang][10]))
+    input_text = get_input_text(option, lang=lang)
+    chosen_ratio = st.sidebar.slider(MESSAGES[f'{lang}.sb.sl'], min_value=10, max_value=50, step=10)/100
 
-    if language=='Cymraeg':
-        st.markdown('### 🌷 Adnodd Creu Crynodebau')
-        st.markdown("#### Rhowch eich testun isod:")
-        option = st.sidebar.radio('Sut ydych chi am fewnbynnu eich testun?', ('Defnyddiwch destun enghreifftiol', 'Rhowch eich testun eich hun', 'Llwythwch ffeil testun i fyny'))
-        if option == 'Defnyddiwch destun enghreifftiol':
-           example_fname = st.sidebar.selectbox('Select example text:', sorted([f for f in os.listdir(EXAMPLES_DIR) if f.startswith('Reviews')]))
-
-           with open(os.path.join(EXAMPLES_DIR, example_fname), 'r', encoding='iso-8859-1') as example_file:
-               example_text = example_file.read()
-
-           input_text = st.text_area('Crynhowch y testun enghreifftiol yn y blwch:', example_text, height=300)
-        
-        elif option == 'Llwythwch ffeil testun i fyny':
-            text = uploadfile()
-            input_text = st.text_area("Crynhoi testun wedi'i uwchlwytho:", text, height=300)
-
-        else:
-            input_text = st.text_area('Teipiwch neu gludwch eich testun yn y blwch testun', '<Rhowch eich testun...>')
-
-        chosen_ratio = st.sidebar.slider('Dewiswch gymhareb y crynodeb [10% i 50%]:', min_value=10, max_value=50, step=10)/100
-        if st.button("Crynhoi👈"):
-            if input_text and input_text!='<Rhowch eich testun (Please enter your text...)>':
-                summary = text_rank_summarize(input_text, ratio=chosen_ratio)
-                if summary:
-                    st.write(text_rank_summarize(input_text, ratio=chosen_ratio))
-                else:
-                    st.write(sent_tokenize(text_rank_summarize(input_text, ratio=0.5))[0])
+    if st.button(MESSAGES[f'{lang}.button']):
+        if input_text and input_text!='<Rhowch eich testun (Please enter your text...)>':
+            summary = text_rank_summarize(input_text, ratio=chosen_ratio)
+            if summary:
+                st.write(text_rank_summarize(input_text, ratio=chosen_ratio))
             else:
-                st.write("Rhowch eich testun...(Please enter your text...)")
-
-    else: #English
-        st.markdown('### 🌷 Welsh Summary Creator')
-        st.markdown("#### Enter your text below:")
-        option = st.sidebar.radio('How do you want to input your text?', ('Use an example text', 'Paste a copied', 'Upload a text file'))
-        if option == 'Use an example text':           
-           example_fname = st.sidebar.selectbox('Select example text:', sorted([f for f in os.listdir(EXAMPLES_DIR)
-                                                  if f.startswith(('en','ex'))]))
-           with open(os.path.join(EXAMPLES_DIR, example_fname), 'r', encoding='iso-8859-1') as example_file:
-               example_text = example_file.read()
-               input_text = st.text_area('Summarise the example text in the box:', example_text, height=300)
-        elif option == 'Upload a text file':
-            text = uploadfile()
-            input_text = st.text_area('Summarise uploaded text:', text, height=300)
+                st.write(sent_tokenize(text_rank_summarize(input_text, ratio=0.5))[0])
         else:
-            input_text = st.text_area('Type or paste your text into the text box:', '<Please enter your text...>', height=300)
-
-        chosen_ratio = st.sidebar.slider('Select summary ratio [10% to 50%]',  min_value=10, max_value=50, step=10)/100
-        if st.button("Summarise👈"):
-            if input_text and input_text not in ['<Please enter your text...>','<Please upload your file ...>']:
-                summary = text_rank_summarize(input_text, ratio=chosen_ratio)
-                if summary:
-                    st.write(text_rank_summarize(input_text, ratio=chosen_ratio))
-                else:
-                    st.write(sent_tokenize(text_rank_summarize(input_text, ratio=0.5))[0])
-            else:
-              st.write('Please select an example, or paste/upload your text')
+            st.write("Rhowch eich testun...(Please enter your text...)")
 
 def run_visualizer():
     # language = st.sidebar.selectbox('Newid iaith (Change language):', ['Cymraeg', 'English'])
@@ -463,8 +415,7 @@ def run_sentiments():
         text = upload_multiple_files()
         input_text = st.text_area('Visualize uploaded text:', text, height=150)
     else:
-        input_text = st.text_area('Type or paste your text into the text box (one review per line):', '<Please type or paste your reviews here...>', height=150)
-    
+        input_text = st.text_area('Type or paste your text into the text box (one review per line):', '<Please type or paste your reviews here...>', height=150)    
     # col1, col2 = st.columns(2)
     # with col1:
     if input_text:
@@ -474,7 +425,6 @@ def run_sentiments():
             plot_sentiments(data[1], fine_grained=False)
         else:
             plot_sentiments(data[1])
-
         # with col2:
         num_examples = st.slider('Number of example [5 to 20%]',  min_value=5, max_value=20, step=5)
         df = pd.DataFrame(data[0], columns =['Review','Polarity', 'Sentiment', 'Subjectivity', 'Category'])
@@ -483,7 +433,6 @@ def run_sentiments():
         st.dataframe(df.head(num_examples))
 
 def run_keyphrase():
-
 # --------------------- Keyword/KeyPhrase ------------------------------
 # Borrowed from https://github.com/streamlit/example-app-bert-keyword-extractor
     st.markdown("**Keyword/KeyPhrase Extraction**")
