a4620f20c42ad798543a2e6f31acb21c642b2ac5
IgnatiusEzeani <igezeani@yahoo.com>
2022-10-16 20:41:52 +0100

    update app

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def get_wordcloud (data, key):
     input_trigrams = [' '.join(g) for g in nltk.ngrams(input_data.split(),3)]
     input_4grams   = [' '.join(g) for g in nltk.ngrams(input_data.split(),4)]
     
-    maskfile = st.selectbox('Select cloud shape:' [f"img/{f}" if f startswith('wc') for f in os.listdir('img')] )   
+    maskfile = st.selectbox('Select cloud shape:' [f"img/{f}" if f.startswith('wc') for f in os.listdir('img')] )
     mask = np.array(Image.open(maskfile))
     # mask = np.array(Image.open('img/wc_welsh_flag.png'))
     # maxWords = st.number_input("Number of words:",
