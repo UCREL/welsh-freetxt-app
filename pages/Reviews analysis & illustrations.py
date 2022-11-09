7444bb2bc4d7540d56a8a38d52bf218b808b6f56
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-11-09 20:06:42 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -1,3 +1,60 @@
 import streamlit as st
 
 st.markdown("# Reviews analysis & illustrations")
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(png_file):
+    with open(png_file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def build_markup_for_logo(
+    png_file,
+    background_position="50% 10%",
+    margin_top="10%",
+    image_width="60%",
+    image_height="",
+):
+    binary_string = get_base64_of_bin_file(png_file)
+    return """
+            <style>
+                [data-testid="stSidebarNav"] {
+                    background-image: url("data:image/png;base64,%s");
+                    background-repeat: no-repeat;
+                    background-position: %s;
+                    margin-top: %s;
+                    background-size: %s %s;
+                }
+            </style>
+            """ % (
+        binary_string,
+        background_position,
+        margin_top,
+        image_width,
+        image_height,
+    )
+
+
+def add_logo(png_file):
+    logo_markup = build_markup_for_logo(png_file)
+    st.markdown(
+        logo_markup,
+        unsafe_allow_html=True,
+    )
+
+
+
+# ----------------
+st.set_page_config(
+     page_title='Welsh Free Text Tool',
+     page_icon='🌐',
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': "https://ucrel.lancs.ac.uk/freetxt/",
+         'Report a bug': "https://github.com/UCREL/welsh-freetxt-app/issues",
+         'About': '''## The FreeTxt tool supports bilingual (English and Welsh) free text data analysis of surveys and questionnaire responses'''
+     }
+ )
+add_logo("img/FreeTxt_logo.png") 
+
