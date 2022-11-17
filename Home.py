62092d354bad98897f3408f067de9ef59fa7eacc
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-11-17 18:19:05 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,7 +14,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
+local_css("style/style.css")
 
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(png_file):
