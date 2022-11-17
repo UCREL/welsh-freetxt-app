81cd9cc10c5bbc11d691dddfb7e93048d3e749c7
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-11-17 22:32:09 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -98,8 +98,14 @@ with st.container():
     left_column, right_column = st.columns([1, 1])
     with left_column:
         st.subheader("[Reviews analysis and illustrations](https://ucrel-welsh-freetxt-app-home-i3gq4l.streamlit.app/Reviews_analysis_&_illustrations)")
-        data_url = read_gif("img/visualization.gif")
+        st.write('''This is one of the key features of the tool and has three core components:
+a. Data View: This allows the user to display and visualize the selected columns from the data file they wish to look at. The user can also dynamically modify the selection or the order of the columns as they wish before performing any other task on the selected columns
+b. Word Cloud: This creates a word cloud from the content of the selected columns. It also allows the user to select the column(s) to build the word cloud from as well as the word cloud type – i.e. 'All words', 'Bigrams', 'Trigrams', '4-grams', 'Nouns', 'Proper nouns', 'Verbs', 'Adjectives', 'Adverbs', 'Numbers'
+c. Key word in Context and Collocation: This extracts the keywords in the review text from the selected columns as well as the contexts within which they appeared in the text allowing the user to adjust the context window. It also shows the collocated words with the selected keywords''')
+
+       
     with right_column:
+        data_url = read_gif("img/visualization.gif")
         st.markdown(
             f'<p style="text-align: center; color: grey;"><a href="https://ucrel-welsh-freetxt-app-home-i3gq4l.streamlit.app/Reviews_analysis_&_illustrations"><img width="200" height="200" src="data:image/gif;base64,{data_url} "></a></p>',
             unsafe_allow_html=True,  
@@ -117,7 +123,7 @@ with st.container():
         
     with right_column:
         st.subheader("[Positive and Negative reviews](https://ucrel-welsh-freetxt-app-home-i3gq4l.streamlit.app/Positive_and_Negative_reviews)")
-           
+        st.write("This feature performs sentiment classification on reviews from selected column(s) and displays a pie chart to visualize the output") 
       
         
 #######################
@@ -127,7 +133,7 @@ with st.container():
     left_column, right_column = st.columns([1, 1])
     with left_column:
         st.subheader("[Generate_a_summary](https://ucrel-welsh-freetxt-app-home-i3gq4l.streamlit.app/Generate_a_summary)")
-        
+        st.write('This tool, adapted from the Welsh Summarization project, produces a basic extractive summary of the review text from the selected columns.')
     with right_column:
         data_url = read_gif("img/summary.gif")
         st.markdown(
@@ -147,7 +153,11 @@ with st.container():
 
     with right_column:
         st.subheader("[Word_Types_and_Relations](https://ucrel-welsh-freetxt-app-home-i3gq4l.streamlit.app/Word_Types_and_Relations)")
-           
+        st.write('''This feature uses the PyMUSAS pipeline on Spacy to generate and display POS (CyTag) tags as well as semantic (USAS) tags. 
+						It currently works on the Ucrel-freetxt-VM as setting up Docker on the Streamlit cloud is a bit complex''')
+
+        text = "Sefydliad cyllidol yw bancwr neu fanc sy'n actio fel asiant talu ar gyfer cwsmeriaid, ac yn rhoi benthyg ac yn benthyg arian. Yn rhai gwledydd, megis yr Almaen a Siapan, mae banciau'n brif berchenogion corfforaethau diwydiannol, tra mewn gwledydd eraill, megis yr Unol Daleithiau, mae banciau'n cael eu gwahardd rhag bod yn berchen ar gwmniau sydd ddim yn rhai cyllidol. Adran Iechyd Cymru."
+  
         
         
 
