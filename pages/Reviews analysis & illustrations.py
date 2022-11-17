e3cc4a31e2255b6488e70e2169e86f4ce754c813
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-11-17 17:45:29 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -1,8 +1,50 @@
+import os
+import string
+import random
+import pandas as pd
+import numpy as np
+import streamlit as st
+from collections import Counter
+import spacy
+from spacy.tokens import Doc
+from spacy.vocab import Vocab
+import nltk
+import en_core_web_sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import networkx as nx
+from PIL import Image
+from textblob import TextBlob
+from nltk import word_tokenize, sent_tokenize, ngrams
+from wordcloud import WordCloud, ImageColorGenerator
+from nltk.corpus import stopwords
+from labels import MESSAGES
+from summarizer_labels import SUM_MESSAGES
+from summa.summarizer import summarize as summa_summarizer
+from langdetect import detect
+nltk.download('punkt') # one time execution
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+from pathlib import Path
+from typing import List
+##word association
+import networkx as nx
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import base64
+
+import circlify ###### pip install circlify
+import plotly.express as px #### pip install plotly.express
+from pyvis.network import Network
+import streamlit.components.v1 as components
+
+
+
+
 import streamlit as st
 import base64
 from PIL import Image
 from labels import MESSAGES
-
 
 
 @st.cache(allow_output_mutation=True)
@@ -62,6 +104,35 @@ st.set_page_config(
  )
 
 
+st.markdown("# Reviews analysis & illustrations")
+add_logo("img/FreeTxt_logo.png") 
+st.write("---")
+
+st.write('''This is one of the key features of the tool and has three core components:
+
+a. Data View: This allows the user to display and visualize the selected columns from the data file they wish to look at. The user can also dynamically modify the selection or the order of the columns as they wish before performing any other task on the selected columns
+
+b. Word Cloud: This creates a word cloud from the content of the selected columns. It also allows the user to select the column(s) to build the word cloud from as well as the word cloud type – i.e. 'All words', 'Bigrams', 'Trigrams', '4-grams', 'Nouns', 'Proper nouns', 'Verbs', 'Adjectives', 'Adverbs', 'Numbers'
+
+c. Key word in Context and Collocation: This extracts the keywords in the review text from the selected columns as well as the contexts within which they appeared in the text allowing the user to adjust the context window. It also shows the collocated words with the selected keywords''')
+
+uploaded_files = st.file_uploader("Choose a .xls file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+
+# Update with the Welsh stopwords (source: https://github.com/techiaith/ataleiriau)
+en_stopwords = list(stopwords.words('english'))
+cy_stopwords = open('welsh_stopwords.txt', 'r', encoding='iso-8859-1').read().split('\n') # replaced 'utf8' with 'iso-8859-1'
+STOPWORDS = set(en_stopwords + cy_stopwords)
+PUNCS = '''!→()-[]{};:'"\,<>./?@#$%^&*_~'''
+pd.set_option('display.max_colwidth',None)
+
+lang='en'
+EXAMPLES_DIR = 'example_texts_pub'
+
+
 # reading example and uploaded files
 def read_file(fname, file_source):
     file_name = fname if file_source=='example' else fname.name
@@ -99,19 +170,174 @@ def get_data(file_source='example'):
     except Exception as err:
         return False, st.error(f'''**UnexpectedFileError:** {err} Some or all of your files may be empty or invalid. Acceptable file formats include `.txt`, `.xlsx`, `.xls`, `.tsv`.''', icon="🚨")
 
-add_logo("img/FreeTxt_logo.png") 
-st.markdown("# Reviews analysis & illustrations")
-st.write("---")
-st.write('''This is one of the key features of the tool and has three core components:
 
-a. Data View: This allows the user to display and visualize the selected columns from the data file they wish to look at. The user can also dynamically modify the selection or the order of the columns as they wish before performing any other task on the selected columns
+def select_columns(data, key):
+    layout = st.columns([7, 0.2, 2, 0.2, 2, 0.2, 3, 0.2, 3])
+    selected_columns = layout[0].multiselect('Select column(s) below to analyse', data.columns, help='Select columns you are interested in with this selection box', key= f"{key}_cols_multiselect")
+    start_row=0
+    if selected_columns: start_row = layout[2].number_input('Choose start row:', value=0, min_value=0, max_value=5)
+    
+    if len(selected_columns)>=2 and layout[4].checkbox('Filter rows?'):
+        filter_column = layout[6].selectbox('Select filter column', selected_columns)
+        if filter_column: 
+            filter_key = layout[8].selectbox('Select filter key', set(data[filter_column]))
+            data = data[selected_columns][start_row:].dropna(how='all')
+            return data.loc[data[filter_column] == filter_key].drop_duplicates()
+    else:
+        return data[selected_columns][start_row:].dropna(how='all').drop_duplicates()
+def get_wordcloud (data, key):
+    # st.markdown('''☁️ Word Cloud''')
+    # cloud_columns = st.multiselect(
+        # 'Which column do you wish to view the word cloud from?', data.columns, list(data.columns), help='Select free text columns to view the word cloud', key=f"{key}_cloud_multiselect")
+    # input_data = ' '.join([' '.join([str(t) for t in list(data[col]) if t not in STOPWORDS]) for col in cloud_columns])
+    # # input_data = ' '.join([' '.join([str(t) for t in list(data[col]) if t not in STOPWORDS]) for col in data])
+    # for c in PUNCS: input_data = input_data.lower().replace(c,'')
 
-b. Word Cloud: This creates a word cloud from the content of the selected columns. It also allows the user to select the column(s) to build the word cloud from as well as the word cloud type – i.e. 'All words', 'Bigrams', 'Trigrams', '4-grams', 'Nouns', 'Proper nouns', 'Verbs', 'Adjectives', 'Adverbs', 'Numbers'
+    st.markdown('''
+    ---
+    
+    ☁️ Word Cloud
+    
+    ---
+    ''')
+    
+    layout = st.columns([7, 1, 4])
+    cloud_columns = layout[0].multiselect(
+        'Which column do you wish to view the word cloud from?', data.columns, list(data.columns), help='Select free text columns to view the word cloud', key=f"{key}_cloud_multiselect")
+    input_data = ' '.join([' '.join([str(t) for t in list(data[col]) if t not in STOPWORDS]) for col in cloud_columns])
+    # input_data = ' '.join([' '.join([str(t) for t in list(data[col]) if t not in STOPWORDS]) for col in data])
+    for c in PUNCS: input_data = input_data.lower().replace(c,'')
+    
+    input_bigrams  = [' '.join(g) for g in nltk.ngrams(input_data.split(),2)]
+    input_trigrams = [' '.join(g) for g in nltk.ngrams(input_data.split(),3)]
+    input_4grams   = [' '.join(g) for g in nltk.ngrams(input_data.split(),4)]
+    #'Welsh Flag': 'img/welsh_flag.png', 'Sherlock Holmes': 'img/holmes_silhouette.png',
+    image_mask = { 'Rectangle': None}
+    
+    maskfile = image_mask[st.selectbox('Select cloud shape:', image_mask.keys(), help='Select the shape of the word cloud')]
+    mask = np.array(Image.open(maskfile)) if maskfile else maskfile
+    # maxWords = st.number_input("Number of words:",
+        # value=300,
+        # step=50,
+        # min_value=50,
+        # max_value=300,
+        # help='Maximum number of words featured in the cloud.',
+        # key=fname
+        # )
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(input_data)
 
-c. Key word in Context and Collocation: This extracts the keywords in the review text from the selected columns as well as the contexts within which they appeared in the text allowing the user to adjust the context window. It also shows the collocated words with the selected keywords''')
+    try:
+        #creating wordcloud
+        wc = WordCloud(
+            # max_words=maxWords,
+            stopwords=STOPWORDS,
+            width=2000, height=1000,
+            relative_scaling = 0,
+            mask=mask,
+            background_color="white",
+            font_path='font/Ubuntu-B.ttf'
+        ).generate(input_data)
+        
+        
+            
+        cloud_type = st.selectbox('Choose cloud category:',
+            ['All words', 'Bigrams', 'Trigrams', '4-grams', 'Nouns', 'Proper nouns', 'Verbs', 'Adjectives', 'Adverbs', 'Numbers'], key= f"{key}_cloud_select")
+        if cloud_type == 'All words':
+            wordcloud = wc.generate(input_data)        
+        elif cloud_type == 'Bigrams':
+            wordcloud = wc.generate_from_frequencies(Counter(input_bigrams))        
+        elif cloud_type == 'Trigrams':
+            wordcloud = wc.generate_from_frequencies(Counter(input_trigrams))        
+        elif cloud_type == '4-grams':
+            wordcloud = wc.generate_from_frequencies(Counter(input_4grams))        
+        elif cloud_type == 'Nouns':
+            wordcloud = wc.generate_from_frequencies(Counter([token.text for token in doc if token.pos_ == "NOUN"]))        
+        elif cloud_type == 'Proper nouns':
+            wordcloud = wc.generate_from_frequencies(Counter([token.text for token in doc if token.pos_ == "PROPN"]))        
+        elif cloud_type == 'Verbs':
+            wordcloud = wc.generate_from_frequencies(Counter([token.text for token in doc if token.pos_ == "VERB"]))
+        elif cloud_type == 'Adjectives':
+            wordcloud = wc.generate_from_frequencies(Counter([token.text for token in doc if token.pos_ == "ADJ"]))
+        elif cloud_type == 'Adverbs':
+            wordcloud = wc.generate_from_frequencies(Counter([token.text for token in doc if token.pos_ == "ADV"]))
+        elif cloud_type == 'Numbers':
+            wordcloud = wc.generate_from_frequencies(Counter([token.text for token in doc if token.pos_ == "NUM"]))
+        else: 
+            pass
+        color = st.radio('Switch image colour:', ('Color', 'Black'), key=f"{key}_cloud_radio")
+        img_cols = ImageColorGenerator(mask) if color == 'Black' else None
+        plt.figure(figsize=[20,15])
+        plt.imshow(wordcloud.recolor(color_func=img_cols), interpolation="bilinear")
+        plt.axis("off")
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
+    except ValueError as err:
+        st.info(f'Oh oh.. Please ensure that at least one free text column is chosen: {err}', icon="🤨")
+class Analysis:
+    def __init__(self, reviews):
+        self.reviews = reviews
 
-uploaded_files = st.file_uploader("Choose a .xls file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    def show_reviews(self, fname):
+        st.markdown(f'''📄 Viewing data: `{fname}`''')
+        st.dataframe(self.reviews)
+        st.write('Total number of reviews: ', len(self.reviews))
+        
+    def show_wordcloud(self, fname):
+        # st.info('Word cloud ran into a technical hitch and we are fixing it...Thanks for you patience', icon='😎')
+        get_wordcloud(self.reviews, fname)
+    
+    def show_kwic(self, fname):
+        plot_kwic(self.reviews, fname)
+# ---------------Checkbox options------------------
+def checkbox_container(data):
+    st.markdown('What do you want to do with the data?')
+    layout = st.columns(2)
+    if layout[0].button('Select All'):
+        for i in data:
+            st.session_state['dynamic_checkbox_' + i] = True
+        st.experimental_rerun()
+    if layout[1].button('UnSelect All'):
+        for i in data:
+            st.session_state['dynamic_checkbox_' + i] = False
+        st.experimental_rerun()
+    for i in data:
+        st.checkbox(i, key='dynamic_checkbox_' + i)
+
+def get_selected_checkboxes():
+    return [i.replace('dynamic_checkbox_','') for i in st.session_state.keys() if i.startswith('dynamic_checkbox_') and 
+    st.session_state[i]]
+
+st.markdown('''🔍 Free Text Visualizer''')
+option = st.sidebar.radio(MESSAGES[lang][0], (MESSAGES[lang][1], MESSAGES[lang][2])) #, MESSAGES[lang][3]))
+if option == MESSAGES[lang][1]: input_data = get_data()
+elif option == MESSAGES[lang][2]: input_data = get_data(file_source='uploaded')
+    # elif option == MESSAGES[lang][3]: input_data = read_example_data()
+else: pass
+    
+status, data = input_data
+if status:
+    if 'feature_list' not in st.session_state.keys():
+            feature_list = ['Data View', 'WordCloud', 'Keyword in Context & Collocation']
+            st.session_state['feature_list'] = feature_list
+    else:
+        feature_list = st.session_state['feature_list']
+    checkbox_container(feature_list)
+    feature_options = get_selected_checkboxes()
+        
+    # With tabbed multiselect
+    filenames = list(data.keys())
+    tab_titles= [f"File-{i+1}" for i in range(len(filenames))]
+    tabs = st.tabs(tab_titles)
+    for i in range(len(tabs)):
+        with tabs[i]:
+            _, df = data[filenames[i]]
+            df = select_columns(df, key=i).astype(str)
+            if df.empty:
+                st.info('''**NoColumnSelected 🤨**: Please select one or more columns to analyse.''', icon="ℹ️")
+            else:
+                analysis = Analysis(df)
+                if not feature_options: st.info('''**NoActionSelected☑️** Select one or more actions from the sidebar checkboxes.''', icon="ℹ️")
+                if 'Data View' in feature_options: analysis.show_reviews(filenames[i])
+                if 'WordCloud' in feature_options: analysis.show_wordcloud(filenames[i])
+                if 'Keyword in Context & Collocation' in feature_options: analysis.show_kwic(filenames[i])
