f8227ecf2a08f35c411314374e599705a1897b67
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-11-24 16:55:41 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -395,7 +395,7 @@ def plot_coll(keyward, collocs):
 ######the network 
     top_collocs_df.insert(1, 'source', keyward)
     G= nx.from_pandas_edgelist(top_collocs_df, source = 'source', target= 'word', edge_attr='freq')
-    nx.draw(G,width=top_collocs_df.freq)
+    nx.draw(G,width=top_collocs_df.freq, node_size=top_collocs_df.freq)
     with tab3:
         st.pyplot()
     
