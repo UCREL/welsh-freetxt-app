a3c51656e16e0762a830deca691954b4581c0bed
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-11-24 14:17:44 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -498,10 +498,10 @@ else: pass
 
 status, data = input_data
 if status:
-  if 'feature_list' not in st.session_state.keys():
+    if 'feature_list' not in st.session_state.keys():
            feature_list = ['Data View', 'WordCloud', 'Keyword in Context & Collocation']
            st.session_state['feature_list'] = feature_list
-  else:
+   else:
      feature_list = st.session_state['feature_list']
      checkbox_container(feature_list)
      feature_options = get_selected_checkboxes()
