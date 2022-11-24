1ccaef663586f33367aef1147b12acf8742847c5
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-11-24 17:00:01 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -395,7 +395,7 @@ def plot_coll(keyward, collocs):
 ######the network 
     top_collocs_df.insert(1, 'source', keyward)
     G= nx.from_pandas_edgelist(top_collocs_df, source = 'source', target= 'word', edge_attr='freq')
-    nx.draw(G,width=top_collocs_df.freq, pos=nx.spring_layout(G, weight='draw_weight'))
+    nx.draw(G,width=top_collocs_df.freq, pos=nx.spring_layout(G, weight='draw_weight'), with_labels=True)
     with tab3:
         st.pyplot()
     
