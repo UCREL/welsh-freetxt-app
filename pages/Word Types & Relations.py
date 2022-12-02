e05026ff166bc6f63f15063440b7c36e102fd4f3
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-12-02 14:57:47 +0000

    Update Word Types & Relations.py

--- pages/Word Types & Relations.py
+++ pages/Word Types & Relations.py
@@ -234,7 +234,7 @@ elif lang_detected == 'en':
 	cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
         tagged_tokens = []
         for token in output_doc:
-        	tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
+        		tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
         
         # # create DataFrame using data
         tagged_tokens_df = pd.DataFrame(tagged_tokens, columns = cols)
