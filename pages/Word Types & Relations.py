9dc9fd78adad0afc1ab19a3d3eab173c5917b06c
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-12-04 14:12:34 +0000

    Update Word Types & Relations.py

--- pages/Word Types & Relations.py
+++ pages/Word Types & Relations.py
@@ -184,8 +184,11 @@ st.write(f"Language detected: '{lang_detected}'")
     
 if lang_detected == 'cy':
         #st.info('The Welsh PyMUSAS tagger is still under construction...', icon='😎')
-	
-	@st.cache(allow_output_mutation=True)
+	with open('img/text_test.txt', 'w+') as txt_reader:
+    		string = st.text_input('ENTER TEXT', value='', max_chars=None, key=None, type='default')
+    		txt_reader.write(string)
+    		st.write(string)	
+
         with open("img/text_test.txt", 'w', encoding='utf-8') as example_text:
             	example_text.write(text_1)
         os.system('cat img/text_test.txt | sudo docker run -i --rm ghcr.io/ucrel/cytag:1.0.4 > welsh_text_example.tsv')
