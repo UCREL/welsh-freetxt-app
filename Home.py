3d85348630de0e404104be391f52bf2cde2ccae7
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-12-06 13:31:40 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -68,10 +68,10 @@ st.set_page_config(
      page_icon='🌐',
      layout="wide",
      initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': "https://ucrel.lancs.ac.uk/freetxt/",
-         'Report a bug': "https://github.com/UCREL/welsh-freetxt-app/issues",
-         'About': '''## The FreeTxt tool supports bilingual (English and Welsh) free text data analysis of surveys and questionnaire responses'''
+    # menu_items={
+     #    'Get Help': "https://ucrel.lancs.ac.uk/freetxt/",
+      #   'Report a bug': "https://github.com/UCREL/welsh-freetxt-app/issues",
+       #  'About': '''## The FreeTxt tool supports bilingual (English and Welsh) free text data analysis of surveys and questionnaire responses'''
      }
  )
 
