21bdb0fb8ae3b4e7b5ba2bfe936e8e39ec2ccfb9
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2022-12-19 20:02:25 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -104,10 +104,10 @@ class Analysis:
             #show(pd.DataFrame(data))
             st.dataframe(self.reviews,use_container_width=True)
             st.write('Total number of reviews: ', len(self.reviews))
-            HtmlFile = open("Visualization.html", 'r', encoding='utf-8')
-            source_code = HtmlFile.read() 
-            print(source_code)
-            components.html(source_code,height = 800)
+            #HtmlFile = open("Visualization.html", 'r', encoding='utf-8')
+            #source_code = HtmlFile.read() 
+            #print(source_code)
+            #components.html(source_code,height = 800)
         
     def show_wordcloud(self, fname):
         # st.info('Word cloud ran into a technical hitch and we are fixing it...Thanks for you patience', icon='😎')
