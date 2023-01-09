ce6a388475d13bb0f72eb2d2f4756f8f373654cc
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-01-09 13:42:32 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -505,17 +505,18 @@ def plot_kwic(data, key):
             with st.expander('Keyword in context'):
                 kwic_instances_df = pd.DataFrame(kwic_instances,
                     columns =['Left context', 'Keyword', 'Right context'])
-                kwic_instances_df.style.set_properties(column='Left context', align = 'center')
+                kwic_instances_df.style.set_properties(column='Left context', align = 'right')
             # subset=['Left context', 'Keyword', 'Right context'],
             # kwic_instances_df
 
                 st.dataframe(kwic_instances_df,use_container_width=True)
-                s1 = dict(selector='th', props=[('text-align', 'right')])
+                s1 = dict(selector='th', props=[('text-align', 'center')])
                 s2 = dict(selector='td', props=[('text-align', 'right')])
                 #s3 = dict(selector='td', props=[('text-align', 'left')])
 		# you can include more styling paramteres, check the pandas docs
                 table = kwic_instances_df.style.set_table_styles([s1,s2]).hide(axis=0).to_html()     
-                st.write(f'{table}', unsafe_allow_html=True)
+                st.dataframe(table,use_container_width=True)
+		#write(f'{table}', unsafe_allow_html=True)
             expander = st.expander('Collocation')
             with expander: #Could you replace with NLTK concordance later?
             # keyword = st.text_input('Enter a keyword:','staff')
