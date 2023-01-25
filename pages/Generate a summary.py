1d5882f0d6f5e6369f9c08f7d975b60d09691871
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-01-25 15:14:27 +0000

    Update Generate a summary.py

--- pages/Generate a summary.py
+++ pages/Generate a summary.py
@@ -165,9 +165,9 @@ def text_rank_summarize(article, ratio):
 # ------------------Summarizer--------------
 def run_summarizer(input_text, num,lang='en'):
 
-    chosen_ratio = st.slider(SUM_MESSAGES[f'{lang}.sb.sl']+str(num), min_value=10, max_value=50, step=10)/100
+    chosen_ratio = st.slider(SUM_MESSAGES[f'{lang}.sb.sl'],key = f'ss+ {num}', min_value=10, max_value=50, step=10)/100
 
-    if st.button(SUM_MESSAGES[f'{lang}.button']+str(num)):
+    if st.button(SUM_MESSAGES[f'{lang}.button'],key = f'bb+ {num}'):
         if input_text and input_text!='<Rhowch eich testun (Please enter your text...)>':
             summary = text_rank_summarize(input_text, ratio=chosen_ratio)
             if summary:
