caf58bda44f0db45601eead26a2c6a7977c2638d
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-01-25 17:23:13 +0000

    Update Generate a summary.py

--- pages/Generate a summary.py
+++ pages/Generate a summary.py
@@ -141,7 +141,7 @@ def get_data(file_source='example'):
     try:
         if file_source=='example':
             example_files = sorted([f for f in os.listdir(EXAMPLES_DIR) if f.startswith('Reviews')])
-            fnames = st.sidebar.selectbox('Select example data file(s)', (example_files,example_files[0]))
+            fnames = st.sidebar.multiselect('Select example data file(s)', example_files,example_files[0])
             
             if fnames:
                 return True, {fname:read_file(os.path.join(EXAMPLES_DIR, fname), file_source) for fname in fnames}
@@ -235,6 +235,7 @@ if status:
         
         #tabs = st.tabs(tab_titles)
         for i in range(len(filenames)):
+            
             
             
             #with tabs[i]:
