ef4a02eca2cc16684a33c8c22db18c21952822e7
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-01-25 17:32:49 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -690,7 +690,7 @@ if st.button('Analysis') or st.session_state.load_state:
     if df.empty:
         st.info('''** 🤨**: Please past text to analyse.''', icon="ℹ️")
     elif len(txt) > 10:
-	st.write("Rhowch eich testun...(Please enter your text in the above textbox)")
+        st.write("Rhowch eich testun...(Please enter your text in the above textbox)")
     else:
        
         tab4, tab5, tab6, tab7 = st.tabs(["📈 Data View", "☁️ Keyword Cloud",'💬 Keyword in Context & Collocation', "📌 Word Tree"])
