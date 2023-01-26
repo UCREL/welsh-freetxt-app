b01479327460f37d959d2e44f34525a03eaf9c09
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-01-26 00:36:54 +0000

    Update Positive and Negative reviews.py

--- pages/Positive and Negative reviews.py
+++ pages/Positive and Negative reviews.py
@@ -306,7 +306,7 @@ if status:
                             
                          convention_df = tt.SampleCorpora.ConventionData2012.get_data()  
                          #convention_df.iloc[0]
-                         nlp = spacy.load('en_core_web_sm-3.2.0')  
+                         nlp = spacy.load('en_core_web_sm')  
                          nlp.max_length = 9000000
                          corpus = tt.CorpusFromPandas(convention_df, 
                              category_col='party', 
