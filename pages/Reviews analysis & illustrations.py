b1d310151ce0a51549413be8d6801e70198f8929
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-01-27 13:36:01 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -446,6 +446,7 @@ def get_wordcloud (data, key):
     s = Bnc_corpus.loc[Bnc_corpus['word'].isin(column1)]
     word_freq = word_freq.merge(s, how='inner', on='word')
     tab2.write(word_freq)
+    keness.run(word_freq,len(words),968267)
 
 # ---------------Checkbox options------------------
 def checkbox_container(data):
