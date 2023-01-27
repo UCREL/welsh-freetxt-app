226a72237079179d793e88233865b57041a86f7a
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-01-27 14:43:08 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -448,7 +448,7 @@ def get_wordcloud (data, key):
     tab2.write(word_freq)
     
     freq = word_freq[['word','freq','f_Reference']].values.tolist()
-    ff = [tuple(r) for r in freq.to_numpy()]
+    ff = [tuple(r) for r in freq]
     ff = list(ff)
 #.apply(tuple, axis=1).tolist()
     tab2.write(ff)
