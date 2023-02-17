c8f8e4dd746d67250e4518905e0582a0ae6d0c56
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-02-17 13:37:31 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -291,7 +291,7 @@ def read_file(fname, file_source):
     column_list = ['date','Date','Dateandtime']
     for col in column_list:
     	if col in data.columns:
-             data[col] = data.col.apply(lambda x: pd.to_datetime(x).strftime('%d/%m/%Y %H:%M'))
+             data[col] = data[col].apply(lambda x: pd.to_datetime(x).strftime('%d/%m/%Y %H:%M'))
 
 
     return True, data
