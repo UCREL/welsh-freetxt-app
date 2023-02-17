cc305bf40910a492b80fa856dd520c3d9b955afe
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-02-17 12:25:22 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -633,7 +633,7 @@ def plot_kwic(data, key):
             # subset=['Left context', 'Keyword', 'Right context'],
             # kwic_instances_df
                 
-                #st.dataframe(dfStyler)       
+                st.dataframe(dfStyler)       
 		
             expander = st.expander('Collocation')
             with expander: #Could you replace with NLTK concordance later?
