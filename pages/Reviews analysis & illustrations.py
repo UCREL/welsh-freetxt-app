1a8e661778444838c3b4fc67a6a1b26cdf65f2f7
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-02-17 13:54:42 +0000

    Update Reviews analysis & illustrations.py

--- pages/Reviews analysis & illustrations.py
+++ pages/Reviews analysis & illustrations.py
@@ -129,8 +129,8 @@ def df_filter(message,df):
 
         slider_1, slider_2 = st.slider('%s' % (message),0,len(df)-1,[0,len(df)-1],1)
 
-        while len(str(df.iloc[slider_1][1]).replace('.0','')) < 4:
-            df.iloc[slider_1,1] = '0' + str(df.iloc[slider_1][1]).replace('.0','')
+        while len(str(df.iloc[slider_1][0]).replace('.0','')) < 4:
+            df.iloc[slider_1,0] = '0' + str(df.iloc[slider_1][1]).replace('.0','')
             
         while len(str(df.iloc[slider_2][1]).replace('.0','')) < 4:
             df.iloc[slider_2,1] = '0' + str(df.iloc[slider_1][1]).replace('.0','')
