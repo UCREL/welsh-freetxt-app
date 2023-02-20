04f8cdaf3731a6cc237b5594f3adc0f09c775fd7
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-02-20 11:20:41 +0000

    Update Word Types & Relations.py

--- pages/Word Types & Relations.py
+++ pages/Word Types & Relations.py
@@ -172,77 +172,23 @@ lang_detected = detect(text)
 st.write(f"Language detected: '{lang_detected}'")
 
    
-if lang_detected == 'cy':
-	#os.system('cat img/data.txt | docker run -i --rm ghcr.io/ucrel/cytag:1.0.4 > welsh_text_example.tsv')
-        #st.info('The Welsh PyMUSAS tagger is still under construction...', icon='😎')
-	#subprocess.run(['bash','run_PyMUSAS.sh'])
-        # # Load the Welsh PyMUSAS rule based tagger
-        # nlp = spacy.load("cy_dual_basiccorcencc2usas_contextual")
 
-        # tokens: List[str] = []
-        # spaces: List[bool] = []
-        # basic_pos_tags: List[str] = []
-        # lemmas: List[str] = []
-
-        # welsh_tagged_file = Path(Path.cwd(), 'welsh_text_example.tsv').resolve()
-
-        # with welsh_tagged_file.open('r', encoding='utf-8') as welsh_tagged_data:
-            # for line in welsh_tagged_data:
-                # line = line.strip()
-                # if line:
-                    # line_tags = line.split('\t')
-                    # tokens.append(line_tags[1])
-                    # lemmas.append(line_tags[3])
-                    # basic_pos_tags.append(line_tags[4])
-                    # spaces.append(True)
-
-        # # As the tagger is a spaCy component that expects tokens, pos, and lemma
-        # # we need to create a spaCy Doc object that will contain this information
-        # doc = Doc(Vocab(), words=tokens, tags=basic_pos_tags, lemmas=lemmas)
-        # output_doc = nlp(doc)
-
-        # # print(f'Text\tLemma\tPOS\tUSAS Tags')
-        # cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
-        # tagged_tokens = []
-        # for token in output_doc:
-            # tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
-        
-        # # create DataFrame using data
-        # tagged_tokens_df = pd.DataFrame(tagged_tokens, columns = cols)
-        # tagged_tokens_df
-	#st.info('The English PyMUSAS tagger is still under construction...', icon='😎')
-	output_doc = nlp(text)
+output_doc = nlp(text)
 	#with st.expander('', expanded=True):
 	#	st.write(f'-\t\tText\t\t\tLemma\t\t\tPOS\t\t\tUSAS Tags')
 	#	for token in output_doc:
 	#		st.write(f'-\t\t{token.text}\t\t\t{token.lemma_}\t\t\t{token.pos_}\t\t\t{token._.pymusas_tags}')
 			
-	cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
-	tagged_tokens = []
-	for token in output_doc:
-		tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
+cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
+tagged_tokens = []
+for token in output_doc:
+	tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
         
         # # create DataFrame using data
-	tagged_tokens_df = pd.DataFrame(tagged_tokens, columns = cols)
-	st.dataframe(tagged_tokens_df,use_container_width=True)
+tagged_tokens_df = pd.DataFrame(tagged_tokens, columns = cols)
+st.dataframe(tagged_tokens_df,use_container_width=True)
 
 		
 
         
     
-elif lang_detected == 'en':
-        #st.info('The English PyMUSAS tagger is still under construction...', icon='😎')
-	output_doc = nlp(text)
-	#with st.expander('', expanded=True):
-	#	st.write(f'-\t\tText\t\t\tLemma\t\t\tPOS\t\t\tUSAS Tags')
-	#	for token in output_doc:
-	#		st.write(f'-\t\t{token.text}\t\t\t{token.lemma_}\t\t\t{token.pos_}\t\t\t{token._.pymusas_tags}')
-			
-	cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
-	tagged_tokens = []
-	for token in output_doc:
-		tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
-        
-        # # create DataFrame using data
-	tagged_tokens_df = pd.DataFrame(tagged_tokens, columns = cols)
-	st.dataframe(tagged_tokens_df,use_container_width=True)
