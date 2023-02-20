af721ee5e54066830857907748a963edd896e555
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-02-20 21:00:11 +0000

    Update Word Types & Relations.py

--- pages/Word Types & Relations.py
+++ pages/Word Types & Relations.py
@@ -179,7 +179,8 @@ if lang_detected == 'cy':
 		}
 
 	response = requests.post('http://ucrel-api-01.lancaster.ac.uk/cgi-bin/pymusas.pl', files=files)
-	st.print(response.text)
+	for line in response.text:
+		st.print(line)
 	
 	#with st.expander('', expanded=True):
 	#	st.write(f'-\t\tText\t\t\tLemma\t\t\tPOS\t\t\tUSAS Tags')
