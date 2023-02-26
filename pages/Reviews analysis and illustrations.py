5e9c09ec309a0e52b7ab64577e1f82b347e71294
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-02-26 12:27:28 +0000

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -174,7 +174,7 @@ class Analysis:
             #slider = st.slider('Select start date', min_value= datetime.strptime(start_date, '%d/%m/%y'),max_value=datetime.strptime(end_date,'%d/%m/%y'),value=datetime.strptime(end_date,'%d/%m/%y'), format=format)
             #options = data['Date'].sort_values().unique().to_list()
             start_d, end_d = st.select_slider('Select a range dates', 
-						      options=data['Date'].to_list(),
+						      options=data['Date'].unique(),
 						      value=(start_date, end_date))
             #mask = (data['Date'] >= data['Date'].min()) & (data['Date'] <= slider)
             #filterdf = data.loc[mask]
