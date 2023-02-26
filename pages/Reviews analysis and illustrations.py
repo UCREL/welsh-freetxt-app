62984b408ca83f63c7d5bbf5b105711f5da70f41
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-02-26 14:16:15 +0000

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -159,8 +159,8 @@ class Analysis:
             selected = grid_response['selected_rows'] 
             df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
             
-            st.table(self.reviews, unsafe_allow_html=True)
-            st.dataframe(self.reviews)
+            st.table(self.reviews)
+            st.dataframe(self.reviews, unsafe_allow_html=True)
 		
             st.write('Total number of reviews: ', len(self.reviews))
 	    
