926f5aced703517949bb2fc891b55215eaa72f24
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-02-27 00:16:40 +0000

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -668,7 +668,7 @@ def plot_kwic(data, key):
 		   #### interactive dataframe
                 gb = GridOptionsBuilder.from_dataframe(kwic_instances_df)
                 gb.configure_column("Left context", editable=True)
-                gb.configure_column("Keyword", cellStyle={'color': 'red', textAlign: 'center'})
+                gb.configure_column("Keyword", cellStyle={ textAlign: 'center'})
                 gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
                 gb.configure_side_bar() #Add a sidebar
                 gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
