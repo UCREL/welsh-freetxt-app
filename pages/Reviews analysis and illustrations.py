b8af7741bb26c77227b21885f62459c64dfb02cc
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-03-15 11:54:04 +0000

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -564,9 +564,10 @@ def plot_coll(keyward, collocs, expander, tab):
     G = nx.from_pandas_edgelist(top_collocs_df, source='source', target='word', edge_attr='freq')
     n = top_collocs_df['freq'].max()
     pos = nx.circular_layout(G)
-    node_sizes = [2000 * freq / n for freq in top_collocs_df['freq']]
+    #node_sizes = [2000 * freq / n for freq in top_collocs_df['freq']]
+#node_size=node_sizes,
     node_colors = ['gray' if node == keyward else 'blue' for node in G.nodes()]
-    nx.draw(G, width=top_collocs_df['freq'], pos=pos, with_labels=True, node_size=node_sizes, node_color=node_colors)
+    nx.draw(G, width=top_collocs_df['freq'], pos=pos, with_labels=True,  node_color=node_colors)
     with tab:
         with expander:
             st.pyplot()
