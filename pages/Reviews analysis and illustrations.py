814b8ade663c0e177ba25ac344c0f90c2903fa83
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-03-15 11:47:07 +0000

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -557,23 +557,20 @@ def plot_collocation(keyword, collocs,expander,tab):
 
 ########the network illistartion
 
+
 def plot_coll(keyward, collocs, expander, tab):
     words, counts = zip(*collocs)
-    top_collocs_df = pd.DataFrame(collocs, columns=['word','freq'])
+    top_collocs_df = pd.DataFrame(collocs, columns=['word', 'freq'])
     top_collocs_df.insert(1, 'source', keyward)
     G = nx.from_pandas_edgelist(top_collocs_df, source='source', target='word', edge_attr='freq')
     n = top_collocs_df['freq'].max()
-
     pos = nx.circular_layout(G)
-
-    node_colors = ['blue' if node == keyward else 'gray' for node in G.nodes()]
-
-    nx.draw(G, width=top_collocs_df.freq, pos=pos, with_labels=True, node_color=node_colors)
-    
+    node_sizes = [2000 * freq / n for freq in top_collocs_df['freq']]
+    node_colors = ['gray' if node == keyward else 'blue' for node in G.nodes()]
+    nx.draw(G, width=top_collocs_df['freq'], pos=pos, with_labels=True, node_size=node_sizes, node_color=node_colors)
     with tab:
         with expander:
             st.pyplot()
-
 
 
  #-------------------------- N-gram Generator ---------------------------
