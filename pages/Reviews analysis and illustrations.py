3c21d4c7c47f58a9c24746ce160b5b872984418f
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-03-17 19:57:47 +0000

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -412,15 +412,14 @@ def Pymsas_tags(text):
         cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
         tagged_tokens = []
         for token in output_doc:
-             tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
+             tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags[0]))
         tagged_tokens_df = pd.DataFrame(tagged_tokens, columns = cols)
-        #tagged_tokens_df['USAS Tags'] = tagged_tokens_df['USAS Tags'].str.split(',').str[0].str.replace('[\[\]"\']', '', regex=True)
-        #merged_df = pd.merge(tagged_tokens_df, pymusaslist, on='USAS Tags', how='left')
-        #merged_df.loc[merged_df['Equivalent Tag'].notnull(), 'USAS Tags'] = merged_df['Equivalent Tag'] 
-        #merged_df = merged_df.drop(['Equivalent Tag'], axis=1)
+        tagged_tokens_df['USAS Tags'] = tagged_tokens_df['USAS Tags'].str.split(',').str[0].str.replace('[\[\]"\']', '', regex=True)
+        merged_df = pd.merge(tagged_tokens_df, pymusaslist, on='USAS Tags', how='left')
+        merged_df.loc[merged_df['Equivalent Tag'].notnull(), 'USAS Tags'] = merged_df['Equivalent Tag'] 
+        merged_df = merged_df.drop(['Equivalent Tag'], axis=1)
         st.dataframe(tagged_tokens_df,use_container_width=True)
-        for token in output_doc:
-              st.write(token._.pymusas_tags[0])
+
 
     
 ###to upload image
