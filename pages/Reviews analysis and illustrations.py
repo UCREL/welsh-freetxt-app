529ed4a0fffded9a6350714c5dc17d6d6f4ee3de
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-03-17 19:43:48 +0000

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -414,6 +414,7 @@ def Pymsas_tags(text):
         for token in output_doc:
              tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
         tagged_tokens_df = pd.DataFrame(tagged_tokens, columns = cols)
+	tagged_tokens_df['USAS Tags'] = tagged_tokens_df['USAS Tags'].str.split(',').str[0].str.replace('[\[\]"\']', '', regex=True)
         #merged_df = pd.merge(tagged_tokens_df, pymusaslist, on='USAS Tags', how='left')
         #merged_df.loc[merged_df['Equivalent Tag'].notnull(), 'USAS Tags'] = merged_df['Equivalent Tag'] 
         #merged_df = merged_df.drop(['Equivalent Tag'], axis=1)
