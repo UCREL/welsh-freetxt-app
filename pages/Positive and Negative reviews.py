0bee525f81a3ed05cfca39cfaea46fb7cc6ced59
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-03-23 09:45:01 +0000

    Update Positive and Negative reviews.py

--- pages/Positive and Negative reviews.py
+++ pages/Positive and Negative reviews.py
@@ -298,6 +298,17 @@ def plot_sentiment_pie(df):
     # create the figure
     fig = go.Figure(data=data, layout=layout)
 
+    # add an event handler to capture the selected data points
+    selected_points = st.plotly_chart(fig)
+    if selected_points:
+        selected_labels = [point['label'] for point in selected_points['points']]
+        subset_df = df[df['Sentiment Label'].isin(selected_labels)]
+        
+        st.dataframe(subset_df, use_container_width= True )
+    
+    # show the plot
+    pio.show(fig)
+
    
     st.plotly_chart(fig)
 
@@ -337,7 +348,7 @@ if status:
                         
                         plot_sentiment(analysis)
                         plot_sentiment_pie(analysis)
-                        st.dataframe(analysis, use_container_width= True )
+                        
                         
                        # text = get_text_sentiments(input_text)
                         #if option == '3 Class Sentiments  (Positive, Neutral, Negative)':
