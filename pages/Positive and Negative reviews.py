139a31bd338a758900d64b92c30c663530f5fd10
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-03-31 02:40:57 +0100

    Update Positive and Negative reviews.py

--- pages/Positive and Negative reviews.py
+++ pages/Positive and Negative reviews.py
@@ -352,7 +352,7 @@ def plot_sentiment_pie(df):
         df = df[df['Sentiment Label'] == sentiment_label]
         st.dataframe(df, use_container_width=True)
 
-
+    st.write(sentiment_label)
     
 
     # update the counts and proportions based on the filtered dataframe
@@ -360,7 +360,7 @@ def plot_sentiment_pie(df):
     proportions = counts / counts.sum()
 
     # update the pie chart data
-    fig.update_traces(labels=proportions.index, values=proportions.values)
+    #fig.update_traces(labels=proportions.index, values=proportions.values)
 
     buffer = io.StringIO()
     fig.write_html(buffer, include_plotlyjs='cdn')
