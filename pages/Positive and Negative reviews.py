73f9f46f199064678b721766ea2309d5cb1b6024
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-03-31 01:52:25 +0100

    Update Positive and Negative reviews.py

--- pages/Positive and Negative reviews.py
+++ pages/Positive and Negative reviews.py
@@ -313,7 +313,8 @@ def plot_sentiment_pie(df):
 
     # create the figure
     fig = go.Figure(data=data, layout=layout)
-    st.plotly_chart(fig)
+    selected_points= st.plotly_chart(fig)
+    st.write(selected_points)
     buffer = io.StringIO()
     fig.write_html(buffer, include_plotlyjs='cdn')
     html_bytes = buffer.getvalue().encode()
