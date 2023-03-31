664c600c6ad760858b651fe441cad433ec1c1bd6
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-03-31 14:50:24 +0100

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -692,7 +692,7 @@ def plot_coll(keyword, collocs, expander, tab):
     edge_widths = [2/ freq for freq in top_collocs_df['freq']]
     edge_colors = top_collocs_df['freq']
 
-    fig = plt.figure(figsize=(8, 8)) # adjust figure size as needed
+    fig = plt.figure(figsize=(6, 6)) # adjust figure size as needed
     ax = fig.add_subplot(111)
     ax.set_aspect('equal')
     ax.set_xlim(-1.2, 1.2) # adjust x-axis limits as needed
