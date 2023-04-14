e74edc1c3762af44b0d253bd9a85c7e82cfb1037
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-14 02:20:33 +0100

    Update Positive and Negative reviews.py

--- pages/Positive and Negative reviews.py
+++ pages/Positive and Negative reviews.py
@@ -220,6 +220,27 @@ st.write("Sentiment polarity per word: ", sentiment_polarity_per_word)
 st.write("Overall sentiment polarity: ", overall_sentiment_polarity)
 st.write("Sentiment: ", sentiment)
  
+text = Text("I love the new iPhone, but the battery life is terrible.")
+
+# Get entities in the text
+entities = text.entities
+
+for entity in entities:
+    entity_sentiment_polarity = []
+    for word in entity.words:
+        word_sentiment_polarity = word.polarity
+        entity_sentiment_polarity.append(word_sentiment_polarity)
+    
+    # Calculate average sentiment polarity for the entity
+    avg_sentiment_polarity = sum(entity_sentiment_polarity) / len(entity_sentiment_polarity)
+    
+    # Scale sentiment polarity to a range of 0-1
+    normalized_sentiment = (avg_sentiment_polarity + 1) / 2
+    
+    # Print entity, average sentiment polarity, and normalized sentiment
+    st.write("Entity: ", entity)
+    st.write("Average sentiment polarity: ", avg_sentiment_polarity)
+    st.write("Normalized sentiment: ", normalized_sentiment)
 
 # --------------------Sentiments----------------------
 
