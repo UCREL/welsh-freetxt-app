1d65315eada85bb9db9dc2bafbe7801ae435f8b8
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-19 17:21:56 +0100

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -867,14 +867,11 @@ def plot_coll_5(keyword, collocs, expander, tab):
     data = {'nodes': nodes, 'links': links}
 
     # Write JSON data to a file
-    with open('temp/graph_data.json', 'w') as f:
+    with open('graph_data.json', 'w') as f:
         json.dump(data, f)
+    	st.write(json.dump(data, f))
+
     
-
-    html_code = "<h1>Hello, World!</h1>"
-
-# Display the HTML code as rendered HTML
-    st.write(html_code, unsafe_allow_html=True)
     # Embed the graph in a web page using D3.js
     html_template = '''
     <html>
@@ -884,7 +881,7 @@ def plot_coll_5(keyword, collocs, expander, tab):
     <body>
         <div id="graph"></div>
         <script>
-            d3.json('temp/graph_data.json').then(function(data) {
+            d3.json('graph_data.json').then(function(data) {
                 var svg = d3.select('#graph')
                     .append('svg')
                     .attr('width', 600)
