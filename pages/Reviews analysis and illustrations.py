7afeb99a2b34adcdd8658708dc3b66ec1e937782
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-19 05:22:00 +0100

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -870,8 +870,7 @@ def plot_coll_5(keyword, collocs, expander, tab):
             <title>D3.js Network Visualization</title>
             <script src="https://d3js.org/d3.v6.min.js"></script>
             <style>
-                #chart svg {
-                    width: 100%;
+                #chart svg {width: 100%;
                     height: 100%;
                 }
                 .node {
