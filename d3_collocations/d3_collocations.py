ee8058bc9c05fa7cd2cdef240761f16a993990fe
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-20 02:06:46 +0100

    Update d3_collocations.py

--- d3_collocations/d3_collocations.py
+++ d3_collocations/d3_collocations.py
@@ -1,25 +1,19 @@
+import requests
+import json
+
 def render_d3_collocations(collocations_data):
     # Download index.html from GitHub
     index_html_url = "https://raw.githubusercontent.com/UCREL/welsh-freetxt-app/main/d3_collocations/index.html"
     index_html_content = requests.get(index_html_url).text
 
+    # Convert collocations_data to a JSON string
+    collocations_data_json = json.dumps(collocations_data)
+
     # Insert collocationsData variable into index_html_content
     index_html_content = index_html_content.replace(
         "<div id=\"graph\"></div>",
-        f"<div id=\"graph\"></div><script>const collocationsData = {json.dumps(collocations_data)};</script>"
+        f"<div id=\"graph\"></div><script>const collocationsData = {collocations_data_json};</script>"
     )
 
-    # Set up the output
-    d3_component = components.declare_component(
-        "d3_component",
-        url="https://d3-component.herokuapp.com",
-    )
+    return index_html_content
 
-    # Call the component with the modified index.html content
-    return d3_component(
-        index_html_content,
-        collocations_data=collocations_data,
-        key="d3_collocations",
-        height=800,
-        width=800,
-    )
