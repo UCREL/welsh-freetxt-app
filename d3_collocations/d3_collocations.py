44be2eced4970027846d78df3b55a991a96fdbdd
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-20 01:11:43 +0100

    Update d3_collocations.py

--- d3_collocations/d3_collocations.py
+++ d3_collocations/d3_collocations.py
@@ -7,13 +7,13 @@ def render_d3_collocations(collocations_data):
     index_html_url = "https://raw.githubusercontent.com/UCREL/welsh-freetxt-app/main/d3_collocations/index.html"
     index_html_content = requests.get(index_html_url).text
 
-    # Convert collocations_data to a JSON string
+    # Embed collocations_data as a JSON string within the HTML content
     collocations_data_json = json.dumps(collocations_data)
+    index_html_content = index_html_content.replace("<!-- collocations_data_placeholder -->", collocations_data_json)
 
     return components.html(
         index_html_content,
         height=800,
         width=800,
-        scripts=f"const collocationsData = {collocations_data_json};"
     )
 
