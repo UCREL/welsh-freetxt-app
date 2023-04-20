2b79259ba39bdcce52d86bbf9b81d28a95fb470c
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-20 01:02:21 +0100

    Update d3_collocations.py

--- d3_collocations/d3_collocations.py
+++ d3_collocations/d3_collocations.py
@@ -1,17 +1,18 @@
-import os
 import streamlit.components.v1 as components
 import requests
+import json
 
 def render_d3_collocations(collocations_data):
     # Download index.html from GitHub
-    index_html_url = "https://raw.githubusercontent.com/UCREL/welsh-freetxt-app/edit/main/d3_collocations/index.html"
+    index_html_url = "https://raw.githubusercontent.com/UCREL/welsh-freetxt-app/main/d3_collocations/index.html"
     index_html_content = requests.get(index_html_url).text
-    
+
+    # Embed collocations_data as a JSON string within the HTML content
+    collocations_data_json = json.dumps(collocations_data)
+    index_html_content = index_html_content.replace("<!-- collocations_data_placeholder -->", collocations_data_json)
+
     return components.html(
         index_html_content,
-        collocations_data=collocations_data,
         height=800,
         width=800,
     )
-
-
