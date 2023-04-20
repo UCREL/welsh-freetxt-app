8a440604cab6ceeb6d584a6eb633022cf6164eff
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-20 01:18:03 +0100

    Update d3_collocations.py

--- d3_collocations/d3_collocations.py
+++ d3_collocations/d3_collocations.py
@@ -10,9 +10,14 @@ def render_d3_collocations(collocations_data):
     # Convert collocations_data to a JSON string
     collocations_data_json = json.dumps(collocations_data)
 
+    # Insert collocationsData variable into index_html_content
+    index_html_content = index_html_content.replace(
+        "<div id=\"graph\"></div>",
+        f"<div id=\"graph\"></div><script>const collocationsData = {collocations_data_json};</script>"
+    )
+
     return components.html(
         index_html_content,
         height=800,
-        width=800,
-        scripts=f"const collocationsData = {collocations_data_json};"
+        width=800
     )
