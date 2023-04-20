b4f45f69dd5b7d81f8477b6372ea11bd52e850c3
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-20 04:39:50 +0100

    Update d3_collocations.py

--- d3_collocations/d3_collocations.py
+++ d3_collocations/d3_collocations.py
@@ -1,6 +1,3 @@
-import requests
-import json
-import streamlit.components.v1 as components
 
 def render_d3_collocations(collocations_data):
     # Download index.html from GitHub
@@ -10,19 +7,8 @@ def render_d3_collocations(collocations_data):
     # Convert collocations_data to a JSON string
     collocations_data_json = json.dumps(collocations_data)
     
-    
     # Replace the collocations_data_placeholder with the actual data
     index_html_content = index_html_content.replace("<!--collocations_data_placeholder-->", json.dumps(collocations_data))
 
-    # Return the HTML component
-    return components.html(index_html_content, height=800, width=800)
-    
-    
-    # Insert collocationsData variable into index_html_content
-    #ndex_html_content = index_html_content.replace(
-      # "<div id=\"graph\"></div>",
-      # f"<div id=\"graph\"></div><script>const collocationsData = {collocations_data_json};</script>"
-  # )
-
-    #return index_html_content
-
+    # Return the HTML content as a string
+    return index_html_content
