780b012358f2b5a2db5f1c3e220ce57bfb85a6e4
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-20 04:27:30 +0100

    Update d3_collocations.py

--- d3_collocations/d3_collocations.py
+++ d3_collocations/d3_collocations.py
@@ -4,7 +4,7 @@ import json
 def render_d3_collocations(collocations_data):
     # Download index.html from GitHub
     index_html_url = "https://raw.githubusercontent.com/UCREL/welsh-freetxt-app/main/d3_collocations/index.html"
-    #index_html_content = requests.get(index_html_url).text
+    index_html_content = requests.get(index_html_url).text
 
     # Convert collocations_data to a JSON string
     collocations_data_json = json.dumps(collocations_data)
