a9143db82f874ba23e8a8710f27eeb10bfdfe355
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-20 04:41:29 +0100

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -889,7 +889,7 @@ def plot_coll_5(keyword, collocs, expander, tab):
 
 from d3_collocations.d3_collocations import render_d3_collocations
 
-def plot_coll_6(keyword, collocs, expander, tab):
+def plot_coll_11(keyword, collocs, expander, tab):
     words, counts = zip(*collocs)
     top_collocs_df = pd.DataFrame(collocs, columns=['word','freq'])
     top_collocs_df.insert(1, 'source', keyword)
@@ -904,7 +904,23 @@ def plot_coll_6(keyword, collocs, expander, tab):
         with expander:
             st.components.v1.html(render_d3_collocations(collocations_data), height=800, width=800)
 
-   
+ from d3_collocations.d3_collocations import render_d3_collocations
+
+def plot_coll_6(keyword, collocs, expander, tab):
+    words, counts = zip(*collocs)
+    top_collocs_df = pd.DataFrame(collocs, columns=['word','freq'])
+    top_collocs_df.insert(1, 'source', keyword)
+    top_collocs_df = top_collocs_df[top_collocs_df['word'] != keyword]
+
+    collocations_data = {
+        "keyword": keyword,
+        "collocations": top_collocs_df.to_dict(orient="records"),
+    }
+
+    with tab:
+        with expander:
+            components.html(render_d3_collocations(collocations_data), height=800, width=800)
+  
 	
 def plot_coll(keyword, collocs, expander, tab):
     words, counts = zip(*collocs)
