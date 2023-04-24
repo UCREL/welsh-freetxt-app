20cb8c14e9569e3e068b3297fd06a9b3c15a287b
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-24 18:17:31 +0100

    Update Reviews analysis and illustrations.py

--- pages/Reviews analysis and illustrations.py
+++ pages/Reviews analysis and illustrations.py
@@ -569,7 +569,7 @@ def get_wordcloud (data, key):
         plt.figure(figsize=[20,15])
         plt.imshow(wordcloud.recolor(color_func=img_cols), interpolation="bilinear")
         plt.axis("off")
-        tab.pyplot()
+        tab2.pyplot()
         plt.figure(figsize=(12, 8))
         plt.imshow(wordcloud_2, interpolation='bilinear')
         plt.axis('off')
