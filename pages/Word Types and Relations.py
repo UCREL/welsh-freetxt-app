6cb8c9e4bc8963cb05cd7b3da46195516de650b2
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-04-24 14:00:39 +0100

    Update Word Types and Relations.py

--- pages/Word Types and Relations.py
+++ pages/Word Types and Relations.py
@@ -302,7 +302,7 @@ if checkbox:
         aspect_ratio = float(logo_height) / float(logo_width)
         logo = ReportLabImage(logo_path, width=100, height=int(100 * aspect_ratio))
         title_text = "Types and Relations Report"
-	title_style = ParagraphStyle("Title", fontSize=24, alignment=TA_LEFT)
+        title_style = ParagraphStyle("Title", fontSize=24, alignment=TA_LEFT)
         title = Paragraph(title_text, title_style)
         header_data = [[logo, title]]
         header_table = Table(header_data)
