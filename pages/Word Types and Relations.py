651da9c33d8f4ba496a46b28c1b20fcbf7f85f00
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-05-02 16:37:08 +0100

    Update Word Types and Relations.py

--- pages/Word Types and Relations.py
+++ pages/Word Types and Relations.py
@@ -244,8 +244,8 @@ elif lang_detected == 'en':
 	english_tagger_pipeline = spacy.load('en_dual_none_contextual')
 # Adds the English PyMUSAS rule-based tagger to the main spaCy pipeline
 	nlp.add_pipe('pymusas_rule_based_tagger', source=english_tagger_pipeline)
-	output_doc = nlp(text)	
-        cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
+	output_doc = nlp(text)
+	cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
         tagged_tokens = []
         for token in output_doc:
              tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags[0]))
