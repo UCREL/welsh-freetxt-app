ad4508faae48090770463b66476457bc0715f11b
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-05-02 16:29:34 +0100

    Update Word Types and Relations.py

--- pages/Word Types and Relations.py
+++ pages/Word Types and Relations.py
@@ -245,17 +245,14 @@ elif lang_detected == 'en':
 # Adds the English PyMUSAS rule-based tagger to the main spaCy pipeline
 	nlp.add_pipe('pymusas_rule_based_tagger', source=english_tagger_pipeline)
 	output_doc = nlp(text)
-	#with st.expander('', expanded=True):
-	#	st.write(f'-\t\tText\t\t\tLemma\t\t\tPOS\t\t\tUSAS Tags')
-	#	for token in output_doc:
-	#		st.write(f'-\t\t{token.text}\t\t\t{token.lemma_}\t\t\t{token.pos_}\t\t\t{token._.pymusas_tags}')
 			
 	cols = ['Text', 'Lemma', 'POS', 'USAS Tags']
 	tagged_tokens = []
 	for token in output_doc:
 		tagged_tokens.append((token.text, token.lemma_, token.tag_, token._.pymusas_tags))
         
-        # # create DataFrame using data
+     
+	pymusaslist = pymusaslist.set_index('USAS Tags')['Equivalent Tag'].to_dict()
 	tagged_tokens_df = pd.DataFrame(tagged_tokens, columns = cols)
 	st.dataframe(tagged_tokens_df,use_container_width=True)
 	
