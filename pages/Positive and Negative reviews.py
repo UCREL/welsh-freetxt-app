334e0da41b1419877d02b30daac0ea0fe97947d2
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-05-04 15:51:22 +0100

    Update Positive and Negative reviews.py

--- pages/Positive and Negative reviews.py
+++ pages/Positive and Negative reviews.py
@@ -686,23 +686,30 @@ if status:
                         template = PageTemplate(id='header_template', frames=frame, onPage=header)
                         doc.addPageTemplates([template])
                         elements = []
-
-    
-       
-        
-
                         # Add a spacer between header and input text
                         elements.append(Spacer(1, 20))
-
+  
 
 			# Add content based on selected checkboxes
                         if data_list_checkbox:
   	                # Add the data list as a table
-                        
-                                 data = df
-                                 table = Table(data)
-                                 elements.append(table)
-                                 elements.append(Spacer(1, 20))
+                                # Add DataFrame as a table
+                             table_data = [df.columns.to_list()] + df.values.tolist()
+                             table = Table(table_data)
+                             table.setStyle(TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+            ('FONTSIZE', (0, 0), (-1, 0), 14),
+
+            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+            ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black)
+        ]))
+                             elements.append(table)
 
                         if sentiment_pie_checkbox:
                         # Add the sentiment pie graph
