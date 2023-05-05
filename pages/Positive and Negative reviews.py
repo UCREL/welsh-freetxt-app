3dfdf409a9066640e7e7c63cae61980500aa42c3
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-05-05 13:13:43 +0100

    Update Positive and Negative reviews.py

--- pages/Positive and Negative reviews.py
+++ pages/Positive and Negative reviews.py
@@ -177,7 +177,7 @@ def read_file(fname, file_source):
     else:
         return False, st.error(f"""**FileFormatError:** Unrecognised file format. Please ensure your file name has the extension `.txt`, `.xlsx`, `.xls`, `.tsv`.""", icon="🚨")
     return True, data
-@st.cache(allow_output_mutation=True)
+@st.cache(suppress_st_warning=True)
 def get_data(file_source='example'):
     try:
         if file_source=='example':
