a7af6cab95c8ef91678c5255d1eafa9e529eb9de
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-05-05 03:00:11 +0100

    Update Generate a summary.py

--- pages/Generate a summary.py
+++ pages/Generate a summary.py
@@ -44,7 +44,7 @@ from io import BytesIO
 from reportlab.lib.pagesizes import letter, landscape, A4
 from reportlab.lib import colors
 from reportlab.lib.enums import TA_CENTER, TA_LEFT
-from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle, Image as ReportLabImage, Spacer, BaseDocTemplate, Frame, PageTemplate
 from reportlab.lib.units import inch
 
