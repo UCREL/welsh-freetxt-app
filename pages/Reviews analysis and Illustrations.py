51dc3768ebf1d1d94df211b7bdb2087aeb8c6b26
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-05-11 02:08:20 +0100

    Update Reviews analysis and Illustrations.py

--- pages/Reviews analysis and Illustrations.py
+++ pages/Reviews analysis and Illustrations.py
@@ -1309,7 +1309,6 @@ if status:
                               ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
 
                              ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-
                                   ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
                                   ('FONTSIZE', (0, 0), (-1, 0), 14),
 
